e63c5557227bd973dbcda33ddc9468c0f0f3ab2d
dhulliath <983915+dhulliath@users.noreply.github.com>
2018-02-03 13:22:20 -0700

    mailing subsystem

--- app/paths/mailer.js
+++ app/paths/mailer.js
@@ -1,31 +1,57 @@
 const local_app = function () {}
 const bodyParser = require('body-parser')
-const nodeMailer = require('nodemailer')
+const nodemailer = require('nodemailer')
 const axios = require('axios')
 
-local_app.prototype.init = function (app) {
-    app.use(bodyParser.urlencoded({extended: false}))
-	//app.use(bodyParser.json())
-	/*
-	let poolConfig = {
-		pool: true,
-		host: 'smtp.example.com',
-		port: 465,
-		secure: true, // use TLS
-		auth: {
-			user: 'username',
-			pass: 'password'
-		}
-	};
+local_app.prototype.transporter = null
 
-	this.mailTransport = nodeMailer.createTransport(poolConfig)*/
+local_app.prototype.init = function (app) {
+	var transporter = nodemailer.createTransport({
+		host: 'smtp.ethereal.email',
+		port: 587,
+		secure: false,
+		auth: {
+			user: 'xnzmgj2nil5c5chl@ethereal.email',
+			pass: 'C9pueduutAM2QhFYqF'
+		}
+	})
+
+	app.use(bodyParser.urlencoded({
+		extended: false
+	}))
 
 	app.post('/mail/send', (req, res) => {
-		if (req.body.gotcha) {
-			res.send('ok')
+		let code = {
+			error: false,
+			honeypot: false
 		}
-		res.send([req.body, req.params])
-		//res.redirect('/emailsuccess/')
+		if (req.body.gotcha) {
+			code.honeypot = true
+		}
+
+		if (code.honeypot) {
+			res.send('okay')
+		}
+		if (code.error) {
+			res.send('error')
+		}
+
+		if (!code.honeypot && !code.error) {
+			let mailOptions = {
+				from: '"Noreply" <noreply@excirworks.com>',
+				replyTo: '"' + req.body.name + '" <' + req.body.replyEmail + '>',
+				to: '"Excir Info" <info@excirworks.com>',
+				subject: 'Site Inquiry: ' + req.body.company,
+				text: 'phone number: ' + req.body.phoneNumber + '\n' + req.body.message
+			}
+			transporter.sendMail(mailOptions, (error, info) => {
+				if (error) {
+					res.send(error)
+				} else {
+					res.redirect('/emailsuccess/')
+				}
+			})
+		}
 	})
 }
 
