b6766387163e07552bfc844e1a6d5ca6fab8c93f
dhulliath <983915+dhulliath@users.noreply.github.com>
2018-02-13 10:26:06 -0700

    image tests

--- cms/generators/article/about.js
+++ cms/generators/article/about.js
@@ -25,7 +25,19 @@
 			href: '/assets/img/circuitgold.svg',
 			$style_size: 8,
 			style: {
-				position: '@@global.styling.position_styles.center',
+				position: '@@global.styling.position_styles.float_left',
+				size: '@@global.styling.size_styles.half'
+			}
+		},
+		{
+			$image_type: 'title',
+			$href_size: 4,
+			image: 'Image with Styling',
+			$href_type: 'image',
+			href: '/assets/img/goldbeaker.svg',
+			$style_size: 8,
+			style: {
+				position: '@@global.styling.position_styles.float_right',
 				size: '@@global.styling.size_styles.half'
 			}
 		},
--- cms/index.js
+++ cms/index.js
@@ -24,6 +24,18 @@
 			markdown: 'Text with Markdown Filter',
 			$text_type: 'textarea',
 			text: 'Excir Works Corp. recovers valuable metals from e-waste and spent catalysts using its proprietary breakthrough technology. Excir’s unique process delivers the final products in highly pure forms (> 99.99%), excluding precious metal products from refining processes. The company aims to apply its unique recycling technology all over the world to become one of the leading producer of precious metals.'
+		},
+		{
+			$image_type: 'title',
+			$href_size: 4,
+			image: 'Image with Styling',
+			$href_type: 'image',
+			href: '/assets/img/goldbeaker.svg',
+			$style_size: 8,
+			style: {
+				position: '@@global.styling.position_styles.center',
+				size: '@@global.styling.size_styles.half'
+			}
 		}
 	],
 	$_content_size: 12,
