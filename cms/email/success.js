519095a833aa4bd42d7767e7a2370dca0150840e
dhulliath <983915+dhulliath@users.noreply.github.com>
2018-02-22 22:03:20 -0700

    tweak some contact stuff

--- cms/email/success.js
+++ cms/email/success.js
@@ -5,6 +5,12 @@
 			markdown: 'Text with Markdown Filter',
 			$text_type: 'textarea',
 			text: 'Thank you for your email! We will get in touch with you ASAP!'
+		},
+		{
+			$markdown_type: 'title',
+			markdown: 'Text with Markdown Filter',
+			$text_type: 'textarea',
+			text: 'To view the emails currently being sent go to:\nhttps://ethereal.email/login\n\nusername: `xnzmgj2nil5c5chl@ethereal.email`\n\npassword: `C9pueduutAM2QhFYqF`'
 		}
 	],
 	$_content_size: 12,
