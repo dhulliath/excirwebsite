deda4372710d3b59ac867b0dfeab7fe404e3b0a6
Peter Olejniczak <dhulliath@gmail.com>
2018-03-02 13:25:15 -0700

    commodities and CAD conversion! woo!

--- app/paths/kitco.js
+++ app/paths/kitco.js
@@ -1,38 +1,14 @@
+//scraper for kito.com, hopefully they never change their table ID's.
+
 const DomParser = require('dom-parser')
 const rp = require('request-promise')
-const tough = require('tough-cookie')
-const money = require('money')
+const fixer = require('fixer-api')
 
 var parser = new DomParser()
-
-//so we get CAD currency
-let cookie = new tough.Cookie({
-    key: "market_currency_ny",
-    value: "CAD",
-    domain: '.www.kitco.com',
-    path: '/',
-    httpOnly: false,
-    maxAge: 31536000
-})
-let cookie_lgqCur = new tough.Cookie({
-    key: 'lgqCur',
-    value: 'USD',
-    domain: '.www.kitco.com',
-    path: '/',
-    httpOnly: false,
-    maxAge: 31536000
-})
-
-var cookiejar = rp.jar();
-cookiejar.setCookie(cookie, 'http://www.kitco.com')
-cookiejar.setCookie(cookie_lgqCur, 'http://www.kitco.com')
-
-
 
 //we cache the data for up to ten minutes, it's daily anyways so whatever.
 const update_time_threshold = 600000 * 6; //ten minutes in milliseconds
 var last_update = 0;
-
 
 var lookup_elements = {
     'gold': 'AU',
@@ -41,14 +17,23 @@ var lookup_elements = {
 }
 
 var prices = {}
+var fixer_data = {}
+
+module.exports.updateCurrency = function () {
+    return fixer.latest().then((data) => {
+        fixer_data = data;
+    })
+}
 
 module.exports.updatePrices = function () {
-    return rp({uri: 'http://www.kitco.com/market/', jar: cookiejar}).then((htmlString) => {
+    return rp({
+        uri: 'http://www.kitco.com/market/'
+    }).then((htmlString) => {
         var dom = parser.parseFromString(htmlString)
         for (key in lookup_elements) {
             prices[key] = {
                 date: dom.getElementById(lookup_elements[key] + '-date').innerHTML,
-                USD: dom.getElementById(lookup_elements[key] + '-bid').innerHTML
+                USD: parseFloat(dom.getElementById(lookup_elements[key] + '-bid').innerHTML)
             }
             //prices[key].CAD = money.convert(prices[key].USD, {from: 'USD', to: 'CAD'})
         }
@@ -59,8 +44,13 @@ module.exports.getPrices = function () {
     return new Promise((resolve, reject) => {
         if ((Date.now() - last_update) > update_time_threshold) {
             last_update = Date.now()
-            module.exports.updatePrices().then(() => {
-                resolve(prices)
+            module.exports.updateCurrency().then(() => {
+                module.exports.updatePrices().then(() => {
+                    for (key in prices) {
+                        prices[key].CAD = round(prices[key].USD * fixer_data.rates.CAD / fixer_data.rates.USD, 2)
+                    }
+                    resolve(prices)
+                })
             })
         } else {
             resolve(prices)
@@ -69,11 +59,13 @@ module.exports.getPrices = function () {
 }
 
 module.exports.test = function () {
-    module.exports.updatePrices().then(() => {
+    module.exports.getPrices().then(() => {
         console.log(prices)
     }).catch((err) => {
         console.log(err)
     })
 }
 
-//module.exportsule.exports = module.exports
+function round(value, decimals) {
+    return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+  }
