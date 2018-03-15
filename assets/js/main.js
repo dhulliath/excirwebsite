5401c7abf5114a92f9e81d15f9887b04cae8ada0
dhulliath <983915+dhulliath@users.noreply.github.com>
2018-03-15 12:41:30 -0600

    fix svg in ie, address formatting, about page edit, remove gold widget

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,20 +5,19 @@ require.config({
 		jquery: "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min",
 	},
 })
-require(['goldwidget'], function (gold) {
+/*require(['goldwidget'], function (gold) {
 	gold.makeGoldWidget('.goldWidget', {
 		'gold': 'Gold',
 		'platinum': 'Platinum',
 		'palladium': 'Palladium'
 	});
+})*/
+require(['svg4everybody.min'], function (svg) {
+	svg4everybody();
 })
 require(['jquery'], function ($) {
 	$(document).ready(function () {
 		console.log('requirejs ready to use')
-		$('.debugbutton').click(function () {
-			$('body').toggleClass('debug')
-		})
-
 		$('nav .burger').click(function () {
 			$(this).parent().parent().toggleClass('is-active');
 		})
